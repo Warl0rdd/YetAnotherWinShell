4f54fb2e86a6c2b22f400296890fcf44e4acae90
WarlOrd <tycoon111222333@gmail.com>
2024-10-25 20:20:54 +0300

    added print_prompt function

--- YetAnotherWinShell/main.c
+++ YetAnotherWinShell/main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "./utils/ascii_art.h"
 
 void startup(void)
 {
 	ascii_art();
+}
+
+void print_prompt(void)
+{
+	char* username = getenv("USERNAME");
+	char* hostname = getenv("COMPUTERNAME");
+
+	printf("%s@%s ~", username, hostname);
+}
+
+void main_loop(void)
+{
+	print_prompt();
 }
 
 int main(int argc, char** argv)
@@ -13,6 +27,8 @@ int main(int argc, char** argv)
 	// TODO config
 
 	startup();
+
+	main_loop();
 
 	// TODO cleanup
 
